d5b646de8a3b334f5afbf1bbba85ae817a205218
Melina Señoráns Pérez <melina.senorans@gmail.com>
2023-02-14 12:58:35 -0300

    mover lo de config.js a server.js

--- server.js
+++ server.js
@@ -20,7 +20,19 @@ const compression = require('compression');
 const logger = require("./winston-logger");
 
 
+const dotenv = require("dotenv");
 
+
+if(process.env.MODE != 'production'){
+    dotenv.config()
+    }
+
+
+const MODE = process.env.MODE;
+const HOST = process.env.HOST;
+const DIRSTATIC = process.env.DIRSTATIC;
+const DATABASEURL = process.env.DATABASEURL;
+const DBPROD = process.env.DBPROD;
 
 
 
